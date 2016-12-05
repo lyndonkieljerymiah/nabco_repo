4a636936944bc11ed26380f5771026ef1df7cbb1
arnold.mercado <arnold.mercado@hotmail.com>
2016-12-05 18:08:17 +0300

    Adding Item Data with Test

---
+++ NabcoPortal.ItemMaster.Domain/IData/IItemData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NabcoPortal.ItemMaster.Domain.Model;
+
+namespace NabcoPortal.ItemMaster.Domain.IData
+{
+    public interface IItemData
+    {
+        Task AddItem(Item item);
+        Task<IEnumerable<Item>> GetItems();
+        Task<Item> GetItem(int Id);
+    }
+}
---
+++ NabcoPortal.ItemMaster.Test/ItemIntegrationTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NabcoPortal.ItemMaster.Data;
+using NabcoPortal.ItemMaster.Data.Data;
+using NabcoPortal.ItemMaster.Domain.IData;
+using NabcoPortal.ItemMaster.Domain.Model;
+
+namespace NabcoPortal.ItemMaster.Test
+{
+    [TestClass]
+    public class ItemIntegrationTest
+    {
+        private ItemContextDb _contextDb;
+        private IItemData _itemData;
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _contextDb = new ItemContextDb();
+            _itemData = new ItemData(_contextDb);
+        }
+
+        [TestMethod]
+        public async Task Can_Add_Item()
+        {
+            var item = Item.Create("100", "Item 1", "Item description 1");
+            await _itemData.AddItem(item);
+            var items = await _itemData.GetItems();
+
+            Assert.AreEqual(1,items.Count());
+        }
+    }
+}
